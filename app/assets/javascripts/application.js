3ac30b35352512344b38cf9464c212f0cf3859ca
Christophe E <cestanol@gmail.com>
2015-02-16 00:12:38 -0500

    Infinite scrolling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,4 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
+  // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
@@ -15,5 +15,6 @@
 //= require bootstrap-sprockets
 //= require jquery_ujs
 //= require masonry/jquery.masonry
+//= require masonry/jquery.infinitescroll.min
 //= require turbolinks
 //= require_tree .
